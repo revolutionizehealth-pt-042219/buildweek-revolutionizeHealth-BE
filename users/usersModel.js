8393aca8ae1092156bc2eb48d5e6900171d47e01
unknown <noah.stoeckel@gmail.com>
2019-04-21 12:57:30 -0400

    writes logic for the update function

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,10 +1,19 @@
 const db = require("../database/dbConfig");
 
-module.exports = { insert };
+module.exports = { insert, update };
 
 async function insert(user) {
   // returns the idea of the newly inserted user
   const [id] = await db("users").insert(user);
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+async function update(id, changes) {
+  await db("users")
+    .where({ id })
+    .update(changes);
   return db("users")
     .where({ id })
     .first();
