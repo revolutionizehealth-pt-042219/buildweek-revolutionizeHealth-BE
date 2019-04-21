2f94b1b1b76cb1114251b36f878f5dd010f0e337
unknown <noah.stoeckel@gmail.com>
2019-04-21 12:56:20 -0400

    write tests for the update function

--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -13,4 +13,27 @@ describe("The Users Model", () => {
       expect(user.password).toBe("test1");
     });
   });
+
+  describe("the update function", () => {
+    it("should return the updated user", async () => {
+      const user = await User.insert({ username: "test1", password: "test1" });
+      const updatedUser = await User.update(user.id, {
+        username: "test2",
+        password: "test2"
+      });
+      expect(updatedUser).not.toBe(undefined);
+    });
+
+    it("should update a specific user", async () => {
+      const user = await User.insert({ username: "test1", password: "test1" });
+      const updatedUser = await User.update(user.id, {
+        username: "test2",
+        password: "test2"
+      });
+      expect(user.username).not.toBe(updatedUser.username);
+      expect(user.password).not.toBe(updatedUser.password);
+      expect(updatedUser.username).toBe("test2");
+      expect(updatedUser.password).toBe("test2");
+    });
+  });
 });
