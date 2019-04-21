9145c43a74e311bd6afde0cca840bc132084becb
unknown <noah.stoeckel@gmail.com>
2019-04-21 15:08:34 -0400

    writes logic and tests for successfully user registration

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -2,16 +2,26 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const { genToken } = require("../auth/tokenService");
-const db = require("../database/dbConfig");
+const Users = require("../users/usersModel");
 
 router.post("/register", async (req, res) => {
-  const user = req.body;
+  let user = req.body;
   if (!user.username || !user.password) {
-    res
-      .status(400)
-      .json({
-        message: "please provide a username and password for registration"
-      });
+    res.status(400).json({
+      message: "please provide a username and password for registration"
+    });
+  } else {
+    try {
+      //Hash the password
+      const hash = bcrypt.hashSync(user.password, 13);
+      user.password = hash;
+      // add the user to the database
+      const newUser = await Users.insert(user);
+      const token = genToken(newUser);
+      res.status(201).json({ token });
+    } catch (e) {
+      res.status(201).json({ error: "could not create user" });
+    }
   }
 });
 
--- users/usersRouter.spec.js
+++ users/usersRouter.spec.js
@@ -9,9 +9,21 @@ beforeEach(() => {
 describe("The Users Router", () => {
   const route = "/api/users";
   describe("POST /register", () => {
-    xit("should send back a JWT on successful registration", async () => {});
-    xit("should return 201 on a successful registration", async () => {});
-    xit("should successfully hash a users password ", async () => {});
+    it("should send back a JWT on successful registration", async () => {
+      const user = { username: "test1", password: "test1" };
+      const res = await req(server)
+        .post(route + "/register")
+        .send(user);
+      expect(res.status).toBe(201);
+      expect(res.body.token).not.toBe(undefined);
+    });
+    it("should return 201 on a successful registration", async () => {
+      const user = { username: "test1", password: "test1" };
+      const res = await req(server)
+        .post(route + "/register")
+        .send(user);
+      expect(res.status).toBe(201);
+    });
     it("should return 400 if password is not provided", async () => {
       const user = { username: "test1" };
       const res = await req(server)
