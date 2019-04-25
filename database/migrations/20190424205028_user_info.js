b27e550014bf1b64e29d542ef66d16aaf9f8a7b9
unknown <noah.stoeckel@gmail.com>
2019-04-24 23:26:20 -0400

    Adds User Info Migrations

---
+++ database/migrations/20190424205028_user_info.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users_info", users_info => {
+    users_info.increments();
+    users_info.foreign("user_id").references("users.id");
+    users_info.string("first_name", 255).notNullable();
+    users_info.string("last_name", 255).notNullable();
+    users_info.string("email", 255).notNullable();
+    users_info.bool("has_insurance").notNullable();
+    users_info.foreign("insurance_id").references("insurance_info.id");
+    users_info.enum("type", ["patient,doctor"]);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("users_info");
+};
