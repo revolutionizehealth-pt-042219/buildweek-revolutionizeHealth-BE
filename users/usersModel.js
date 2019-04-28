f6f1f19015775d8d8ba14833632d8c44a072e36b
unknown <noah.stoeckel@gmail.com>
2019-04-28 14:00:50 -0400

    heroku

--- users/usersModel.js
+++ users/usersModel.js
@@ -47,8 +47,8 @@ async function insert(userInfo) {
   }
 
   //insert userInfo into users_info
-  console.log(userProfile);
-  console.log(insurance_id);
+  console.log("userprofile", userProfile);
+  console.log("insurance_id", insurance_id);
   const [user_info_id] = await db("users_info").insert(userProfile, ["id"]);
 
   return db
