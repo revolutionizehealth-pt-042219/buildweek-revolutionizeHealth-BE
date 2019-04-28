46c247437befe7287dedb22ecaa206615866d2f6
unknown <noah.stoeckel@gmail.com>
2019-04-28 14:52:16 -0400

    add users seed

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -5,9 +5,24 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("users").insert([
-        { id: 1, username: "test1", password: "" },
-        { id: 2, username: "test2", password: "" },
-        { id: 3, username: "test2", password: "" }
+        {
+          id: 1,
+          username: "test1",
+          password:
+            "$2a$13$ImRiWpOfZgOxV/hH6ArJ2e/k1GYo/gro50GhAD7WO55pHJB0C0m/u"
+        },
+        {
+          id: 2,
+          username: "test2",
+          password:
+            "$2a$13$MHLAANhAEJaiTg7JONm52.BGNloUVjwGmCH/.BFNQXvYWD5V4ResC"
+        },
+        {
+          id: 3,
+          username: "test2",
+          password:
+            "$2a$13$iV0QBeoZFeXxVRyfUsWYlOM3EquTFl2jFZt32HTeyDk4IQooFIE.C"
+        }
       ]);
     });
 };
