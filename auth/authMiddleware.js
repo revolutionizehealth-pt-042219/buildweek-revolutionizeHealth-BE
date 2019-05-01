fe4f89a86028261a70bad2f13d8ba8b32330b4b6
unknown <noah.stoeckel@gmail.com>
2019-05-01 16:00:16 -0400

    changes header to 'Authentication'

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -12,7 +12,7 @@ module.exports = {
 
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get("Authorization");
+  const token = req.get("Authentication");
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
