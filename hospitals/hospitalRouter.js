090bb9cf13a6539f065a950d9e692064f99f465d
unknown <noah.stoeckel@gmail.com>
2019-05-03 17:45:31 -0400

    Add put existing hospital

--- hospitals/hospitalRouter.js
+++ hospitals/hospitalRouter.js
@@ -30,7 +30,6 @@ router.post("/", authenticate, async (req, res) => {
   const hospitalInfo = req.body;
   try {
     const newHospital = await Hospitals.insert(hospitalInfo);
-    console.log("newHospital", newHospital);
     res.status(201).json(newHospital);
   } catch (e) {
     dumpError(e);
@@ -38,29 +37,16 @@ router.post("/", authenticate, async (req, res) => {
   }
 });
 
-// router.put("/:procedureId", authenticate, async (req, res) => {
-//   const changes = req.body;
-//   const { procedureId } = req.params;
-//   try {
-//     const [procedure] = await Procedures.getById(procedureId);
-//     const { hospital_id, doctor_id } = procedure;
-//     //if the user who made the procedure is not the one trying to edit
-//     if (procedure.id !== req.decoded.id) {
-//       res.status(401).json({ error: "Not Authorized To Edit Procedure" });
-//     } else {
-//       const newProcedure = await Procedures.update(
-//         procedureId,
-//         changes,
-//         hospital_id,
-//         doctor_id
-//       );
-//       console.log("newProcedure", newProcedure);
-//       res.status(201).json(newProcedure);
-//     }
-//   } catch (e) {
-//     dumpError(e);
-//     res.status(500).json({ error: "could not create procedure" });
-//   }
-// });
+router.put("/:hospitalId", authenticate, async (req, res) => {
+  const changes = req.body;
+  const { hospitalId } = req.params;
+  try {
+    const updatedHosptial = await Hospitals.update(hospitalId, changes);
+    res.status(201).json(updatedHosptial);
+  } catch (e) {
+    dumpError(e);
+    res.status(500).json({ error: "could not create procedure" });
+  }
+});
 
 module.exports = router;
